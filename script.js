e314ff8c780741afeb38d072512fbe041570044b
Jang <jang1902er@gmail.com>
2024-05-22 11:06:44 +0700

    commit

--- script.js
+++ script.js
@@ -58,3 +58,71 @@ function Ex6Function(){
 
     document.getElementById("result_ex6").innerHTML=output;
 }
+
+function Ex7Function(){
+    let age=Number(document.getElementById("input_age_ex7").value);
+    let output="";
+    if(age>0 && age<120){
+        output="is age"
+    }else output="not age"
+    document.getElementById("result_ex7").innerHTML=output;
+}
+
+function Ex8Function(){
+    let side=Number(document.getElementById("input_ex8").value)
+    let area=Math.PI*side*side;
+    document.getElementById("result_ex8").innerHTML="area of square is: "+area;
+}
+
+function Ex9Function(){
+    let a=document.getElementById("input_a_ex9").value;
+    let b=document.getElementById("input_b_ex9").value;
+    let area=a*b;
+    document.getElementById("result_ex9").innerHTML="area of rectangle is: "+area;
+}
+
+function Ex10Function(){
+    let a=document.getElementById("input_a_ex10").value;
+    let b=document.getElementById("input_b_ex10").value;
+    let area=a*b*0.5;
+    document.getElementById("result_ex10").innerHTML="area of triangle is: "+area;
+}
+
+function Ex11Function(){
+    let a=document.getElementById("input_a_ex11").value;
+    let b=document.getElementById("input_b_ex11").value; 
+    let x=-1*b/a;
+    document.getElementById("result_ex11").innerHTML="result x of "+a+"x+"+b+" is "+x;
+}
+
+function Ex12Function(){
+    let a=Number(document.getElementById("input_a_ex12").value);
+    let b=Number(document.getElementById("input_b_ex12").value); 
+    let c=Number(document.getElementById("input_c_ex12").value); 
+
+    let x1=0,x2=0;
+    let delta=b*b-4*a*c
+    if(delta<0) x1=x2="null"
+    else if(delta==0) x1=x2=-b/2*a
+    else {
+        x1=(-b+Math.sqrt(delta))/2*a;
+        x2=(-b-Math.sqrt(delta))/2*a;
+    }
+    console.log(delta);
+    document.getElementById("result_ex12").innerHTML="result of "+a+"x²+"+b+"x+"+c+" is x1= "+x1+" and x2= "+x2;
+}
+function Ex13Function(){
+    let a=document.getElementById("input_ex13").value;
+    let fahrenheit=1.8*a +32;
+    document.getElementById("result_ex13").innerHTML="°F: "+fahrenheit;
+}
+function Ex14Function(){
+    let a=document.getElementById("input_ex14").value;
+    let x=a*3.2808399;
+    document.getElementById("result_ex14").innerHTML="Feet: "+x;
+}
+function Ex15Function(){
+    let a=document.getElementById("input_ex15").value;
+    let x=a*3.2808399;
+    document.getElementById("result_ex15").innerHTML="Feet: "+x;
+}
